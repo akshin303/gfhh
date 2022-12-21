eba1eda15eace9cce05554f689751b0bdcec760d
akshin303 <agshinha@code.edu.az>
2022-12-21 12:36:45 +0400

    first comment

---
+++ 123.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace ConsoleApp17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car new1 = new Car(50);
+
+
+            Console.WriteLine(new1.CurrentFuel);
+
+
+
+
+
+
+
+
+            Console.WriteLine("Please enter a number: ");
+            int count = int.Parse(Console.ReadLine());
+            Employee employee = new Employee(0, "reis");
+            Employee[] employees = new Employee[count];
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine("Please enter employee Salary");
+                employee.Salary = int.Parse(Console.ReadLine());
+                Console.WriteLine("Please enter employee Position");
+                employee.Position = Console.ReadLine();
+                employee = new Employee(employee.Salary, employee.Position);
+                string data = employee.ToString();
+                employees[i] = employee;
+
+            }
+            for (int i = 0; i < employees.Length; i++)
+            {
+                Console.WriteLine(employees[i]);
+            }
+
+
+
+
+
+            Console.WriteLine("Please type 'y' or 'n' in the console ");
+            string input = Console.ReadLine();
+            do
+            {
+                Console.WriteLine("Please type 'y' or 'n': ");
+                input = Console.ReadLine();
+
+            } while (input != "y" && input != "n");
+
+            if (input == "y")
+            {
+                Console.WriteLine("Please enter full name: ");
+                string fullname = Console.ReadLine();
+                Console.WriteLine();
+                
+
+
+            }
+            if (input == "n")
+            {
+                Console.WriteLine("end");
+            }
+        }
+    }
+} 
